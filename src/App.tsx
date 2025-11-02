24f1798a718d551c0e5d1aa2018f26f00c079580
BossBabyGit <2flyingfox2@gmail.com>
2025-11-02 19:45:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const PRIZES: Record<number, number> = {
 };
 
 
-const SEASON_END = new Date("2025-10-31T23:59:59Z");
+const SEASON_END = new Date("2025-11-30T23:59:59Z");
 
 /* ---------- Hooks ---------- */
 function useCountdown(targetDate: Date) {
